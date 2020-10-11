31a930290745e1013d9a021bda7196145f84cc1f
Ashutosh Varma <ashutoshvarma11@live.com>
2020-10-11 15:26:54 +0530

    Update telegram-bot.js

--- bot/telegram-bot.js
+++ bot/telegram-bot.js
@@ -1,13 +1,25 @@
 var TelegramBot = require('node-telegram-bot-api');
-var token = process.env.TOKEN;
-const bot = new TelegramBot(token, {polling: true});
 const axios = require('axios');
+
+const options = {
+  webHook: {
+    // Port to which you should bind is assigned to $PORT variable
+    // See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
+    port: process.env.PORT || 5000
+    // you do NOT need to set up certificates since Heroku provides
+    // the SSL certs already (https://<app-name>.herokuapp.com)
+    // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
+  }
+};
+
+const TOKEN = process.env.TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN';
+const bot = new TelegramBot(token, options);
 
 
 bot.on('message' , (msg) =>{
     var text = msg.text.toString();
     axios
-    .post('http://728eb4fa62ae.ngrok.io/find_news' , {
+    .post('https://glacial-depths-57528.herokuapp.com/find_news' , {
         data : text
     })
     .then(res =>{
